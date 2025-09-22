17f2a7ca9c2b07db80c457bbf83dc97a7245538e
Alana Duda <alanaduda74@gmail.com>
2025-09-22 19:07:26 -0300

    adicionando botão limpar com ícone

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const listaDeAmigos = document.getElementById('listaAmigos')
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById('amigo');
+    const nome = input.value;
+    const li = document.createElement('li');
+    li.textContent = nome;
+    listaDeAmigos.appendChild(li);
+    listaDeNomes.push(nome)
+    input.value = '';
+}
+
+function sortearAmigo() {
+    const numeroAleatorio = parseInt(Math.random() * listaDeNomes.length + 1);
+    const amigoSorteado = listaDeNomes[numeroAleatorio - 1];
+    alert(`O amigo sorteado foi ${amigoSorteado} !`);
+}
+
+function limpar() {
+    listaDeNomes = [];
+    listaDeAmigos.innerHTML = '';
+}
